7bffac9a8ec05d87c9e072ac9f70223aa5b9af60
Unknown <matheuspgarcia@gmail.com>
2018-06-15 18:57:34 -0300

    Added google search

--- RandomQuotes/RandomQuotes/QuoteViewController.swift
+++ RandomQuotes/RandomQuotes/QuoteViewController.swift
@@ -14,6 +14,7 @@ class QuoteViewController: UIViewController {
 
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var moreAboutButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,12 +28,30 @@ class QuoteViewController: UIViewController {
 
         let quoteText = "\"\(currentQuote.text)\""
         let authorText = "- \(currentQuote.author)"
+        let moreAboutText = "More about \(currentQuote.author)"
 
         quoteLabel.text = quoteText
         authorLabel.text = authorText
+        moreAboutButton.setTitle(moreAboutText, for: .normal)
+        self.view.layoutIfNeeded()
     }
 
     @IBAction func updateQuote(_ sender: UIButton) {
         quoteModel.updateQuote()
+    }
+
+    @IBAction func moreAbout(_ sender: Any) {
+
+        var author = quoteModel.quote.author
+        author = author.replacingOccurrences(of: " ", with: "+")
+
+        let urlString = "http://www.google.com/search?q=\(author)"
+
+        let url = URL(string: urlString)
+
+        if let url = url {
+            UIApplication.shared.canOpenURL(url)
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
     }
 }
