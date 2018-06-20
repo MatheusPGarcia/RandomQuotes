e80d37e43be25db5f17803feada651b750976ce0
Unknown <matheuspgarcia@gmail.com>
2018-06-20 14:26:49 -0300

    Added AppleWatch Target

---
+++ RandomQuotes/RandomQuotes Watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  RandomQuotes Watch Extension
+//
+//  Created by Matheus Garcia on 20/06/18.
+//  Copyright © 2018 Matheus Garcia. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+// swiftlint:disable all
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+}
+// swiftlint:enable all
